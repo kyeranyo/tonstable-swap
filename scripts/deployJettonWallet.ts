af9ad1798aa91f05ff86094c32555c8f0587b015
Nguyễn Gia Huy <85545963+Huy-8bit@users.noreply.github.com>
2023-06-20 17:05:17 +0700

    update test

---
+++ scripts/deployJettonWallet.ts
@@ -0,0 +1,27 @@
+// import { toNano } from 'ton-core';
+
+// import { compile, NetworkProvider } from '@ton-community/blueprint';
+import { JettonWallet } from '../wrappers/JettonWallet';
+import { Address, beginCell, toNano } from 'ton-core';
+import { compile, NetworkProvider } from '@ton-community/blueprint';
+
+import fs = require('fs');
+
+
+
+export async function run(provider: NetworkProvider) {
+    const wallet = provider.open(JettonWallet.createFromConfig({
+        ownerAddress: provider.sender().address as Address,
+        minterAddress: provider.sender().address as Address,
+        walletCode: await compile('JettonWallet')
+    }, await compile('JettonWallet')));
+
+    await wallet.sendDeploy(provider.sender(), toNano('0.05'));
+
+
+    await provider.waitForDeploy(wallet.address);
+
+    fs.writeFileSync('jettonWalletAddress.txt', wallet.address.toString());
+
+    // run methods on `wallet`
+}
--- wrappers/JettonWallet.compile.ts
+++ wrappers/JettonWallet.compile.ts
@@ -2,5 +2,5 @@ import { CompilerConfig } from '@ton-community/blueprint';
 
 export const compile: CompilerConfig = {
     lang: 'func',
-    targets: ['contracts/wallet.fc'],
+    targets: ['contracts/jetton_wallet.fc'],
 };
