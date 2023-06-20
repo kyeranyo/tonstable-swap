6d184138cb61b32cf4ebe1f48373f6d3268593fe
Nguyễn Gia Huy <85545963+Huy-8bit@users.noreply.github.com>
2023-06-20 17:52:18 +0700

    update code

--- jest.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from 'jest';
-
-const config: Config = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    setupFilesAfterEnv: ['./jest.setup.js']
-};
-
-export default config;
